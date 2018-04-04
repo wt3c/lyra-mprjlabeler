b6f26621416911c472ee33a3bc8c631b419eb984
samambaman <felipe.gomes.ferreira@gmail.com>
2018-04-04 10:04:10 -0300

    renomeando script de startup da aplicacao

--- app.sh
+++ app.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # GUNICORN_CMD_ARGS = variável de configuração do Gunicorn
-gunicorn mprjlabeler.wsgi:application --log-file -
+gunicorn mprjlabeler.wsgi:application --bind=0.0.0.0:8080 --log-file -
