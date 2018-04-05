22c421aa363ec55c8062c2d52ee4c8ff24f863c9
samambaman <felipe.gomes.ferreira@gmail.com>
2018-04-05 10:24:37 -0300

    Adicao do newrelic e do binario do psycopg2

--- mprjlabeler/wsgi.py
+++ mprjlabeler/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
+from newrelic.agent import WSGIApplicationWrapper
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mprjlabeler.settings")
 
 application = get_wsgi_application()
+
+application = WSGIApplicationWrapper(application)
