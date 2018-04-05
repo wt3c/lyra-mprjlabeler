83249151899f24ccd764ee55c5e8fc19df4a102e
samambaman <felipe.gomes.ferreira@gmail.com>
2018-04-05 09:45:01 -0300

    mudanca da conexao do banco de dados para environment

--- mprjlabeler/settings.py
+++ mprjlabeler/settings.py
@@ -79,10 +79,7 @@ WSGI_APPLICATION = 'mprjlabeler.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config()
 }
 
 
@@ -138,8 +135,5 @@ RAVEN_CONFIG = {
 
 if "AMBIENTE" in os.environ and os.environ["AMBIENTE"] == "producao":
     # Parametros para rodar no heroku
-    DATABASES = {
-        'default': dj_database_url.config()
-    }
     DEBUG = False
 
