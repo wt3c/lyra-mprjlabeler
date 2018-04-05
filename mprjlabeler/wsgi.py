ff0765258efeaf21cf086e4080b54385eec5920e
samambaman <felipe.gomes.ferreira@gmail.com>
2018-04-05 16:06:20 -0300

    melhoria no wsgi para newrelic

--- mprjlabeler/wsgi.py
+++ mprjlabeler/wsgi.py
@@ -8,12 +8,16 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
-from newrelic.agent import WSGIApplicationWrapper
-
+import newrelic.agent
 from django.core.wsgi import get_wsgi_application
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+newrelic.agent.initialize(os.path.join(BASE_DIR, 'newrelic.ini'))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mprjlabeler.settings")
 
 application = get_wsgi_application()
 
-application = WSGIApplicationWrapper(application)
+application = newrelic.agent.WSGIApplicationWrapper(application)
