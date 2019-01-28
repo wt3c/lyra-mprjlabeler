62f3b8cda6e3ea6dd48d777dd9d27484a9e50571
Felipe Ferreira <felipe.gomes.ferreira@gmail.com>
2019-01-28 16:25:27 -0200

    alteração do app.sh para saber instanciar celery workers

--- app.sh
+++ app.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-# GUNICORN_CMD_ARGS = variável de configuração do Gunicorn
-gunicorn mprjlabeler.wsgi:application --bind=0.0.0.0:8080 --log-file -
+if [ -z "$WORKER" ]
+then
+    # GUNICORN_CMD_ARGS = variável de configuração do Gunicorn
+    gunicorn mprjlabeler.wsgi:application --bind=0.0.0.0:8080 --log-file -
+else
+    celery -A mprjlabeler worker -l info
+fi
