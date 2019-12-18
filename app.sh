8c7594eb8b502d3f19959aaf5c2e00f59b41790e
Felipe Gomes Vieira Ferreira <felipe.gomes.ferreira@gmail.com>
2019-12-18 11:16:38 -0300

    Atualização de timeout ignorante

--- app.sh
+++ app.sh
@@ -2,7 +2,7 @@
 if [ -z "$WORKER" ]
 then
     # GUNICORN_CMD_ARGS = variável de configuração do Gunicorn
-    gunicorn mprjlabeler.wsgi:application --bind=0.0.0.0:8080 --log-file -
+    gunicorn mprjlabeler.wsgi:application --timeout=2000 --bind=0.0.0.0:8080 --log-file -
 else
     celery -A mprjlabeler worker -l info
 fi
